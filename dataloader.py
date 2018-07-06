09dc3b5dc57bea78ab3a9d73ba84654b02b7709a
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-06 14:21:56 +0200

    dataloader: load_cifar implemented

--- dataloader.py
+++ dataloader.py
@@ -5,11 +5,19 @@ import torchvision.transforms as transforms
 def load_cifar(save_path):
     '''
     Check if the CIFAR10 dataset already exists in the directory "save path". If not, the CIFAR10 dataset is downloaded.
-    Returns trainset and testset of CIFAR10. Applied transformations: ToTensor() and normalization.
+    Returns trainset, testset and classes of CIFAR10. Applied transformations: ToTensor() and normalization.
 
     :param save_path: subdirectory, where the CIFAR10 dataset should be load from or downloaded to
-    :return:
+    :return: trainset, testset, classes
     '''
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.49139968, 0.48215841, 0.44653091), \
                                                                                 (0.24703223, 0.24348513, 0.26158784))])
+
+    trainset = datasets.CIFAR10(root=save_path, train=True, transform=transform, download=True)
+    testset = datasets.CIFAR10(root=save_path, train=False, transform=transform, download=True)
+
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+    return trainset, testset, classes
+
