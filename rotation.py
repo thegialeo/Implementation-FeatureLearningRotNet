67cfd061d079c24f7375c0ea581559e253da853d
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-06 23:14:32 +0200

    rotation: apply function implement

--- rotation.py
+++ rotation.py
@@ -54,3 +54,18 @@ def rot270(image):
     rot_im = vert_im.permute(0, 2, 1)
 
     return rot_im
+
+
+def apply(func, M):
+    '''
+    Applies a function on valid arguments or list of arguments.
+
+    :param func: The function that we want to apply.
+    :param M: The argument or list of arguments that will be passed to the function.
+    :return: The result that the function returns.
+    '''
+
+    tList = [func(m) for m in torch.unbind(M, dim=0)]
+    res = torch.stack(tList, dim=0)
+
+    return res
