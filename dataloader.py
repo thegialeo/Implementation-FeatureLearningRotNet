9137e63b72eb1c7f9737b5a965c27c1d65457e21
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-06 14:00:10 +0200

    dataloader: init load_cifar function

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,13 @@
 import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+
+def load_cifar(save_path):
+    '''
+    Check if the CIFAR10 dataset already exists in the directory "save path". If not, the CIFAR10 dataset is downloaded.
+    Returns trainset and testset of CIFAR10.
+
+    :param save_path:
+    :return:
+    '''
+    pass
