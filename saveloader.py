a593a6bbfc3a548c3ade83d346b805ee7077662a
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-06 18:39:50 +0200

    saveloader: init load_variable function, add description

--- saveloader.py
+++ saveloader.py
@@ -12,3 +12,12 @@ def save_variable(var_list, filename):
     with open(filename, 'w') as f:
         pickle.dump(var_list, f)
 
+
+def load_variable(filename):
+    '''
+    Load variables from a .pkl file.
+
+    :param filename: name of the file to load the variables from
+    :return: list of variables loaded from .pkl file
+    '''
+    #with open()
