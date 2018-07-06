d2edee0c82825725f45c7f26a13ac1246a6b8b80
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-06 18:33:23 +0200

    saveloader: save_variable function implemented

--- saveloader.py
+++ saveloader.py
@@ -1,5 +1,14 @@
 import pickle
 
 
-def save_variable():
-    pass
+def save_variable(var_list, filename):
+    '''
+    Takes a list of variables and save them in a .pkl file.
+
+    :param var_list: a list of variables to save
+    :param filename: name of the file the variables should be save in
+    :return: None
+    '''
+    with open(filename, 'w') as f:
+        pickle.dump(var_list, f)
+
