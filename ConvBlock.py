1b3d02de786cdcdbf5016b2403c6ee7dbfbfdffe
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-07 16:37:36 +0200

    convblock: forward pass implemented

--- ConvBlock.py
+++ ConvBlock.py
@@ -16,3 +16,7 @@ class conv_block(nn.Module):
         self.layers.add_module('ConvB1', bb.basic_block(in_channels, out_channels_block1, kernel_size_block1))
         self.layers.add_module('ConvB2', bb.basic_block(out_channels_block1, out_channels_block2, kernel_size_block2))
         self.layers.add_module('ConvB3', bb.basic_block(out_channels_block2, out_channels_block3, kernel_size_block3))
+
+
+    def forward(self, x):
+        return self.layers(x)
