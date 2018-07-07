1212da36cee844df7d0ee56b611597f637d2a756
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-07 16:36:43 +0200

    convblock: __init__ function implemented

--- ConvBlock.py
+++ ConvBlock.py
@@ -1,6 +1,18 @@
 import torch.nn as nn
-
+import BasicBlock as bb
 
 
 class conv_block(nn.Module):
-    pass
+    '''
+    A convolutional block consists of 3 Basic Blocks.
+    '''
+
+
+    def __init__(self, in_channels, out_channels_block1, out_channels_block2, out_channels_block3, kernel_size_block1, \
+                 kernel_size_block2, kernel_size_block3):
+        super(conv_block, self).__init__()
+
+        self.layers = nn.Sequential()
+        self.layers.add_module('ConvB1', bb.basic_block(in_channels, out_channels_block1, kernel_size_block1))
+        self.layers.add_module('ConvB2', bb.basic_block(out_channels_block1, out_channels_block2, kernel_size_block2))
+        self.layers.add_module('ConvB3', bb.basic_block(out_channels_block2, out_channels_block3, kernel_size_block3))
