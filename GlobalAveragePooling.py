2753a1520aaddb8f6557629360190c6e4a5c3c3e
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-07 16:44:05 +0200

    GlobalAveragePooling implemented

--- GlobalAveragePooling.py
+++ GlobalAveragePooling.py
@@ -1,6 +1,17 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 
 class GlobalAveragePooling(nn.Module):
-    pass
+
+
+    def __init__(self):
+        super(GlobalAveragePooling, self).__init__()
+
+
+    def forward(self, feat):
+
+        num_channels = feat.size(1)
+
+        return F.avg_pool2d(feat, (feat.size(2), feat.size(3))).view(-1, num_channels)
