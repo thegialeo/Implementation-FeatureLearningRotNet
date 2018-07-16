e78d970850ebd8c318f69645cc1687d2902119ff
Xenovortex <Leo.Nguyen@gmx.de>
2018-07-16 21:27:49 +0200

    trainer: add classification task training for network and classifier

--- trainer.py
+++ trainer.py
@@ -1,8 +1,9 @@
+import torch.nn as nn
 import rotation as rtt
 import evaluater as eva
 
 
-def train(num_epoch, num_class, net, trainloader, validloader, optimizer, criterion, class_names, classifier=None,
+def train(num_epoch, num_class, net, trainloader, validloader, criterion, optimizer, class_names, classifier=None,
           conv_block_num=None, epoch_offset=0, rot=None, max_accuracy=0, best_epoch=0, use_paper_metric=False):
     """
     Train a neural network.
@@ -19,8 +20,8 @@ def train(num_epoch, num_class, net, trainloader, validloader, optimizer, criter
     :param net: neural network that should be trained
     :param trainloader: the training set wrapped by a loader
     :param validloader: the validation set wrapped by a loader
+    :param criterion: the criterion to compute the loss
     :param optimizer: the optimization method used for training
-    :param criterion: criterion by which to compute the loss
     :param class_names: list of class names corresponding the labels
     :param classifier: optional argument, if provided, the classifier will be attached to the feature map of the x-th
     convolutional block of the neural network (where x = conv_block_num) and trained for classification task
@@ -51,8 +52,18 @@ def train(num_epoch, num_class, net, trainloader, validloader, optimizer, criter
             if rot == None:
                 inputs, labels = inputs.to(device), labels.to(device)
                 if classifier == None:
-                    pass
+                    optimizer.zero_grad()
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
                 else:
+                    optimizer.zero_grad()
+                    feats = net(inputs, out_feat_keys=[net.all_feat_names[conv_block_num]])
+                    outputs = classifier(feats)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
             else:
                 rot_inputs, class_labels, rot_labels = rtt.create_rot_batch(inputs, labels, rot=rot)
                 rot_inputs, rot_labels = rot_inputs.to(device), rot_labels.to(device)
